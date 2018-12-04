e00290ae5ec56a9a62a6495e5d3c52850091ee12
young-zy <youngzhaozhou@gmail.com>
2018-12-04 17:54:28 +0800

    delete redundant files

--- 68.cpp
+++
@@ -1,8 +0,0 @@
-#include<bits/stdc++.h>
-
-using namespace std;
-
-int main(){
-    printf("fahr = 100, celsius = %d",5* (100-32) / 9);
-    return 0;
-}
--- 92.cpp
+++
@@ -1,8 +0,0 @@
-#include<bits/stdc++.h>
-
-using namespace std;
-
-int main(){
-    printf("* * * *\n * * *\n  * *\n   *");
-    return 0;
-}
