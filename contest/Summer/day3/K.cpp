8bb47aa6d8768b2c02444be0e4037d8611224f1c
young-zy <youngzhaozhou@gmail.com>
2019-07-17 20:38:35 +0800

    20190717

--- contest/Summer/day3/K.cpp
+++ contest/Summer/day3/K.cpp
@@ -7,6 +7,16 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
+    long long s;
+    cin>>s;
+    long long ans = 0;
+    for(long long i = sqrt(2*s) + 1; i>=1;i--){
+        if((2*s +i -i*i) % (2 *i) == 0 && (2*s +i -i*i) > 0){
+            ans = i;
+            break;
+        }
+    }
+    long long start = (2*s - ans*ans + ans)/(2*ans);
+    cout<<start<<" "<<ans+start-1;
     return 0;
 }
