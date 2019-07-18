2edf11a6f6b202bcd3bbfd1c40411d88d49751ff
young-zy <youngzhaozhou@gmail.com>
2019-07-18 20:36:10 +0800

    20190718

--- contest/Summer/day3/F.cpp
+++ contest/Summer/day3/F.cpp
@@ -4,9 +4,38 @@
 
 using namespace std;
 
+struct node{
+    int x;
+    int depth;
+};
+
+priority_queue<node> pq;
+
+int limit[200005];
+int a[200005];
+
+
+vector<int> e[200005],dep[200005];
+
+void dfs(int a, int fa, int d ){
+
+}
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
+    int n;
+    cin>>n;
+    for(int i = 1; i<=n ; i++){
+        cin>>a[i];
+        limit[a[i]+1]++;
+    }
+    for(int i = 1; i<n;i++){
+        int x,y;
+        cin>>x>>y;
+        e[x].push_back(y);
+        e[y].push_back(x);
+    }
+
     return 0;
 }
