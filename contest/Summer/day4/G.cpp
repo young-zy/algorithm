a8c30ce9c3399cb3eae9772515c662dd2a828b10
young-zy <youngzhaozhou@gmail.com>
2019-07-19 20:30:08 +0800

    20190719

--- contest/Summer/day4/G.cpp
+++ contest/Summer/day4/G.cpp
@@ -7,99 +7,71 @@
 
 using namespace std;
 
-class Segtree{
-    public:
-        long long n;
-        long long ans[Maxn * 4];        //树
-        long long a[Maxn];              //数据数组
-        long long tag[Maxn * 4];        //懒标记
+long long tree[200005 * 4];
 
-        inline long long left_son(long long p){
-            return p << 1;          //p*2
-        }
+void pushup(int tindex){
+    tree[tindex] = max(tree[tindex<<1],tree[(tindex<<1)|1]); 
+}
 
-        inline long long right_son(long long p){
-            return p << 1 | 1;      //p * 2 + 1
-        }
+void build(int treeindex,int l,int r){
+    if(l == r){
+        tree[treeindex] = 0;
+        return;
+    }
+    int m = (l+r)>>1;
+    if(l<=m){
+        build(treeindex<<1,l,m);
+    } 
+    if(m < r){
+        build((treeindex<<1)|1,m+1,r);
+    }
+}
 
-        inline void upward_update(long long p){         //向上更新
-            ans[p] = ans[left_son(p)] + ans[right_son(p)];
-        }
+void modify(int treeindex,int l, int r, int index,long long y){
+    if(l == r){
+        tree[treeindex] = y;
+        return;
+    }
+    int m = (l+r)>>1;
+    if(index <= m){
+        modify(treeindex << 1,l,m,index, y);
+    }else{
+        modify((treeindex << 1) | 1,m+1,r,index, y);
+    }
+    pushup(treeindex);
+}
 
-        inline void build(long long p , long long l ,long long r){       //建树
-            tag[p] = 0;                                     //初始化懒标记为0
-            if(l == r){                                     //若左标记等于右标记，说明为叶子节点
-                ans[p] = a[l];
-                return;
-            }
-            long long mid = (l + r) >> 1;                   //计算中值
-            build(left_son(p),l,mid);                       //建左子树
-            build(right_son(p), mid+1,r);                   //建右子树
-            upward_update(p);                               //更新节点p
-        }
-        inline void tag_update(long long p, long long l,long long r,long long k){
-            tag[p] += k;                                //标记这个节点每个子节点均被加k，但未被更新
-            ans[p] += k * (r - l + 1);                  //更新该节点结果值
-        }
-        inline void downward_update(long long p,long long l,long long r){
-            long long mid = (l + r) >> 1;
-            tag_update(left_son(p),l,mid,tag[p]);       //更新左子节点的tag和值
-            tag_update(right_son(p),mid+1,r,tag[p]);    //更新右子节点的tag和值
-            tag[p] = 0;                                 //将懒标记清零
-        }
-        inline void update(long long p,long long l0, long long r0, long long l, long long  r,long long k){
-            //l0,r0 为需要更改的区间范围
-            //l,r   为当前所在的区间范围
-            long long mid = (l + r) >> 1;
-            if(l0 <= l && r <= r0){         //范围完全覆盖
-                tag_update(p,l,r,k);        
-                return;
-            }
-            downward_update(p,l,r);         //范围未完全覆盖，向下更新
-            if(l0 <= mid){
-                update(left_son(p),l0,r0,l,mid,k);
-            }
-            if(r0 > mid){
-                update(right_son(p),l0,r0,mid+1,r,k);
-            }
-            upward_update(p);
-        }
-
-        long long query(long long l0,long long r0,long long l, long long r,long long p){
-            //l0,r0 为需要更改的区间范围
-            //l,r   为当前所在的区间范围
-            long long mid = (l + r) >> 1;
-            long long res = 0;
-            if(l0 <= l && r0 >=r){              //若完全覆盖则直接返回节点值
-                return ans[p];          
-            }
-            downward_update(p,l,r);             //向下更新懒标记
-            if(l0 <= mid){
-                res += query(l0,r0,l,mid,left_son(p));
-            }
-            if(r0 > mid){
-                res += query(l0,r0,mid+1,r,right_son(p));
-            }
-            return res;
-        }
-        
-};
-
-class Segtree seg;
+long long query(int treeindex,int l,int r,int ll,int rr){
+    if(ll<=l && rr>=r){
+        return tree[treeindex];
+    }
+    int m = (l+r)>>1;
+    long long ans = -INF;
+    if(ll <= m){
+        ans = max(ans,query(((treeindex<<1)),l,m,ll,rr));
+    }
+    if(m < rr){
+        ans = max(ans,query(((treeindex<<1)|1),m+1,r,ll,rr));
+    }
+    return ans;
+}
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int m,d;
+    long long m,d;
     cin>>m>>d;
+    long long length = 0;
+    build(1,1,m);
+    long long last = 0;
     for(int i = 0; i<m ; i++){
         char ch;
         int b;
         cin>>ch>>b;
         if(ch == 'Q'){
-
+            cout<<(last = query(1,1,m,length-b+1,length))<<endl;
         }else{
-            
+            modify(1,1,m,++length,(last+b +d)%d);
         }
     }
     return 0;
