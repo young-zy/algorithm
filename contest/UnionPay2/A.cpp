3e6b235d9b71d0e03332fc3f7b18e030e5ebdb4c
young-zy <youngzhaozhou@gmail.com>
2019-07-22 12:18:52 +0800

    20190722

--- contest/UnionPay2/A.cpp
+++ contest/UnionPay2/A.cpp
@@ -4,9 +4,40 @@
 
 using namespace std;
 
+char ch[305][305];
+
+long long a[305][305];
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
+    int t;
+    cin>>t;
+    for(int te = 0; te < t ;te++){
+        int n,m;
+        cin>>n>>m;
+        for(int i = 0; i <n ; i++){
+            for(int j = 0; j<m ; j++){
+                cin>>ch[i][j];
+                if(ch[i][j] == '.'){
+                    a[i][j] = 1;
+                }else{
+                    a[i][j] = 0;
+                }
+            }
+        }
+        long long maxnum = 0;
+        for(int i = 0; i<n ; i++){
+            for(int j = 0; j<m ; j++){
+                if(a[i][j] == 1){
+                    if(i>0 && j > 0){
+                        a[i][j] = min(min(a[i-1][j-1],a[i][j-1]),a[i-1][j])+1;
+                    }
+                }
+                maxnum = max(maxnum,a[i][j]*a[i][j]);
+            }
+        }
+        cout<<maxnum<<endl;
+    }
     return 0;
 }
--- contest/UnionPay2/B.cpp
+++ contest/UnionPay2/B.cpp
@@ -1,12 +1,76 @@
 #include<bits/stdc++.h>
 #define endl "\n"
 #define INF 0x3f3f3f3f
+#define mod 1000000007
 
 using namespace std;
+
+ long long f(long long n){
+     long long ans;
+     long long a = n;
+     long long b = a+1;
+     long long c = 2*a+1;
+    if(a % 2 == 0){
+        a/=2;
+    }else{
+        b/=2;
+    }
+    if(a % 3 == 0){
+        a/=3;
+    }else if(b % 3 == 0){
+        b /=3;
+    }else{
+        c /=3;
+    }
+    ans = ((((a%mod)*(b%mod))% mod) *(c%mod))%mod;
+    return ans;
+}
+
+ long long b( long long n, long long d){
+     long long ans= f(n/d)-f(n/(d+1));
+    if(ans < 0){
+        ans += mod;
+    }
+    return ans;
+}
+
+ long long a(long long n){
+     long long ans = 0;
+    for(long long i = 1; i<=sqrt(n) ; i++){
+        ans = ans + ((i%mod)*(b(n,i)%mod))%mod;
+        ans %= mod;
+    }
+    return ans;
+}
+
+long long c( long long n){
+     long long ans = 0;
+    for( long long i = 1; i<=(n/((long long)sqrt(n)+1)); i++){
+        ans = ans + ((((i%mod) * (i%mod))%mod) * ((n/i)%mod))%mod;
+        ans %= mod;
+    }
+    return ans;
+}
+
+long long sigma2(long long n){
+    return (c(n)+a(n))%mod;
+}
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
+    unsigned long long n,m;
+    cin>>n>>m;
+    long long x = ((n*(n+1)>>1)%mod * n%mod)%mod;
+    long long y = ((m*(m+1)>>1)%mod * m%mod)%mod;
+    x -= sigma2(n);
+    if(x < 0){
+        x+=mod;
+    }
+    y -= sigma2(m);
+    if(y < 0){
+        y+=mod;
+    }
+    cout<<(x*y)%mod;
     return 0;
 }
